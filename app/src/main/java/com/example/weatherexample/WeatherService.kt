3b10f35d739a5a541a9ff41dfeed6af14f432a87
DiegoAVega <dvegacampos02@gmail.com>
2024-03-09 08:25:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weatherexample/WeatherService.kt
@@ -0,0 +1,38 @@
+package com.example.weatherexample
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("appid") appId: String,
+        @Query("q") city: String,
+        @Query("units") units: String,
+    ): WeatherResponse
+
+}
+
+object RetrofitServiceFactory {
+
+    fun makeRetrofitService(): WeatherService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherService::class.java)
+    }
+
+}
+
+class WeatherResponse (
+    val main: Main
+)
+
+class Main(
+    val temp: Double
+)
