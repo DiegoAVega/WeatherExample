9a0c3f9229438247166abb6fa7f5c5bce0e7c790
DiegoAVega <dvegacampos02@gmail.com>
2024-03-10 07:44:11 -0500

    se agrego edit text y el boton de busqueda

--- app/src/main/java/com/example/weatherexample/MainActivity.kt
+++ app/src/main/java/com/example/weatherexample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.weatherexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
@@ -11,15 +13,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textViewTemperature: TextView
+    private lateinit var textViewLocationName: TextView
+    private lateinit var editTextLocation: EditText
+    private lateinit var buttonSearch: Button
+    private val weatherService = RetrofitServiceFactory.makeRetrofitService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewTemperature = findViewById(R.id.textViewTemperature)
-        val weatherService = RetrofitServiceFactory.makeRetrofitService()
+        editTextLocation = findViewById(R.id.editTextTextLocation)
+        buttonSearch = findViewById(R.id.buttonSearch)
+        textViewLocationName = findViewById(R.id.textViewLocationName)
+        buttonSearch.setOnClickListener {
+            val city = editTextLocation.text.toString()
+            textViewLocationName.text = city
+            getTemperature(city)
+        }
+        getTemperature("Lima")
+    }
+
+    fun getTemperature(city: String) {
         lifecycleScope.launch {
-            val response = weatherService.getWeather("bd6abe5bcdddf0126b47e2f85aa84476","Lima","metric")
-            textViewTemperature.text = response.main.temp.toString()
+            val response = weatherService.getWeather(
+                "bd6abe5bcdddf0126b47e2f85aa84476",
+                city,
+                "metric"
+            )
+            textViewTemperature.text = "${response.main.temp.toString()}°C"
         }
     }
 
